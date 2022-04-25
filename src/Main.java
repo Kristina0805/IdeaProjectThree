f0005e642e458d0dd8b4ce8f6a9bcfd6b8d8ff8d
Kristina0805 <kristya25.95@mail.ru>
2022-04-25 21:45:37 +0500

    replacement

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-
         int check = 250;
         int replenishment = 1350;
-        boolean ReplenishmentOfMoreThanAThousand = true;
+        boolean replenishmentOfMoreThanAThousand = true;
 
         int bonus;
-        if (ReplenishmentOfMoreThanAThousand) {
+        if (replenishmentOfMoreThanAThousand) {
             bonus = 1;
         } else {
             bonus = 0;
         }
         int accrual = replenishment / 100;
-        int FinalScore = check + replenishment + accrual;
+        int finalScore = check + replenishment + accrual;
 
-        System.out.println("Итоговый счет: " + FinalScore);
+        System.out.println("Итоговый счет: " + finalScore);
         System.out.println("Начисленный бонус: " + accrual);
 
 
