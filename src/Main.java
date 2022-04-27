107200e61825a454f394cb61595b075aabf35210
Kristina0805 <kristya25.95@mail.ru>
2022-04-27 17:55:07 +0500

    change

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
+
 public class Main {
     public static void main(String[] args) {
 
         int check = 250;
-        int replenishment = 1350;
-        boolean replenishmentOfMoreThanAThousand = true;
+        int replenishment = 1500;
 
         int bonus;
-        if (replenishmentOfMoreThanAThousand) {
+        if (replenishment >= 1000) {
             bonus = 1;
         } else {
             bonus = 0;
+            System.out.println("Бонусы не начисляются! Пополнение счета меньше 1000 руб.");
         }
-        int accrual = replenishment / 100;
+
+        int accrual = (replenishment / 100) * bonus;
         int finalScore = check + replenishment + accrual;
 
         System.out.println("Итоговый счет: " + finalScore);
         System.out.println("Начисленный бонус: " + accrual);
-
 
     }
 
